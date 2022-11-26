426a2094d7d11955bbc50b8e1be5315516bea2bf
CodeNinja-tech <elo48858@gmail.com>
2022-11-26 12:14:55 +0100

    ️`🛠️ update:` Constructed An Array From Scratch

---
+++ building_arrays.py
@@ -0,0 +1,40 @@
+class Array:
+    def __init__(self):
+        self.length: int = 0
+        self.data: dict = {}
+
+    @property
+    def len(self):
+        return self.length
+
+    @property
+    def show_list(self):
+        return self.data
+
+    def get(self, index: int):
+        return self.data[str(index)] if str(index) in self.data.keys() else None
+
+    def push(self, item):
+        _next = self.len
+        self.data[str(_next)] = item
+        self.length += 1
+
+    def extend(self, items: list):
+        for item in items:
+            self.data[str(self.len)] = item
+            self.length += 1
+
+    def pop(self):
+        del self.data[str(self.len - 1)]
+        self.length -= 1
+
+
+new_arr = Array()
+new_arr.push('hi')
+new_arr.push('yes!!')
+new_arr.push('this is conquered')
+new_arr.pop()
+new_arr.extend(['wow', 'this', 'is', 'cool'])
+new_arr.pop()
+print(new_arr.get(0))
+print(new_arr.show_list)
