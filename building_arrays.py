c38d02edbccd59476f0442985cf6b05dde396a36
CodeNinja-tech <elo48858@gmail.com>
2022-11-28 13:46:51 +0100

    `🛠️ update:` Added Delete Method

--- building_arrays.py
+++ building_arrays.py
@@ -30,15 +30,30 @@ class Array:
         self.length -= 1
         return last_item
 
+    def shift_items(self, index: int):
+        for i in range(index, self.len - 1):
+            self.data[str(i)] = self.data[str(i+1)]
+
+        del self.data[str(self.len - 1)]
+        self.length -= 1
+
+    def delete(self, index: int):
+        item = self.data[str(index)]
+        self.shift_items(index)
+        return item
+
 
 new_arr = Array()
 new_arr.push('hi')
 new_arr.push('yes!!')
 new_arr.push('this is conquered')
-print(new_arr.pop())
+# print(new_arr.pop())
 new_arr.extend(['wow', 'this', 'is', 'cool'])
-print(new_arr.pop())
+# print(new_arr.pop())
 
-print(new_arr.get(0))
+# print(new_arr.get(0))
 print(new_arr.show_list)
+print(new_arr.len)
+
+new_arr.delete(1)
 print(new_arr.len)
